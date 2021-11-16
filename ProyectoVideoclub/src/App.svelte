ea76b29c00e4e0cd7330d856b10d8ce9872d16bc
Henry <henryjeje@hotmail.com>
2021-11-16 17:44:55 +0100

    function customers

--- ProyectoVideoclub/src/App.svelte
+++ ProyectoVideoclub/src/App.svelte
@@ -74,3 +74,71 @@
             addFilm();
         }
     };
+     // FUNCIONES CUSTOMERS
+
+     let customer = {
+        name: "",
+        lastName: "",
+        Category_preference: "",
+        img: "",
+    };
+
+    let customers = [];
+
+    const loadCustomer = async () => {
+        const querySnapshot = await getDocs(collection(db, "Clientes"));
+        let docs = [];
+        querySnapshot.forEach((doc) => {
+            docs.push({ ...doc.data(), id: doc.id });
+        });
+
+        customers = [...docs];
+        console.log(customers);
+    };
+
+    loadCustomer();
+    
+
+    
+
+    const clearCustomer = () => {
+        customer = {
+            name: "",
+            lastName: "",
+            category_preference: "",
+            img: "",
+        };
+        edit = false;
+    };
+
+    const addCustomer = async () => {
+        await addDoc(collection(db, "Clientes"), customer);
+        await loadCustomer();
+        clearCustomer();
+    };
+
+    const saveCustomer = async () => {
+        await updateDoc(doc(db, "Clientes", customer.id), customer);
+        await loadCustomer();
+        clearCustomer();
+    };
+
+    const editCustomer = (cu) => {
+        customer = Object.assign({}, cu);
+        edit = true;
+    };
+
+    const deleteCustomer = async (id) => {
+        await deleteDoc(doc(db, "Clientes", id));
+        await loadCustomer();
+    };
+
+    const onSubmitHandlerCustomer = (j) => {
+        if (edit) {
+            console.log("save..");
+            saveCustomer();
+        } else {
+            addCustomer();
+        }
+    };
+</script>
